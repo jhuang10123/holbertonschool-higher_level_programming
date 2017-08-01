bf9ed48a267d3adc99940680c3b4eab6e77e05ed
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-08-01 10:52:55 -0700

    rm extra loop

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(10):
-        if i == 9 and j == 9:
-            print("{:d}{:d}".format(i, j))
-        else:
-            print("{:d}{:d}".format(i, j), end=", ")
+for i in range(100):
+    if i < 10:
+        print("0", end="")
+    if i == 99:
+        print("{:d}".format(i))
+    else:
+        print("{:d}".format(i), end=", ")
