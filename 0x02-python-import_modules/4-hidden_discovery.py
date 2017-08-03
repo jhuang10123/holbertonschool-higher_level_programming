4a3fc38126e14cbaa31b8d02e6db58f58ea7059f
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-08-03 14:22:13 -0700

    iterate list directly

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,6 +4,11 @@ if __name__ == "__main__":
 
     hidden_list = dir(hidden_4)
 
+    for i in hidden_list:
+        if i[:2] != "__":
+            print(i)
+'''
     for i in range(len(hidden_list)):
         if hidden_list[i][:2] != "__":
             print(hidden_list[i])
+'''
