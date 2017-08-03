997dbff16f06388cade4b756c24a254f9ab22372
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-08-03 14:19:23 -0700

    printed all names

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,4 +2,8 @@
 if __name__ == "__main__":
     import hidden_4
 
+    hidden_list = dir(hidden_4)
 
+    for i in range(len(hidden_list)):
+        if hidden_list[:2] != "__":
+            print(hidden_list[i])
