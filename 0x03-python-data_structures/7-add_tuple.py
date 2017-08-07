a7517cc4c7bbb4a00d6309a540dfebdaaeb3a08d
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-08-07 15:00:48 -0700

    removed extra code

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,11 +4,8 @@
 def add_tuple(tuple_a=(), tuple_b=()):
 
     sumlist = []
-    a = list(tuple_a)
-    b = list(tuple_b)
-
-    list_a = a.extend([0, 0])
-    list_b = b.extend([0, 0])
+    a = list(tuple_a) + [0, 0]
+    b = list(tuple_b) + [0, 0]
 
     for i in range(2):
         sumlist.append(a[i] + b[i])
