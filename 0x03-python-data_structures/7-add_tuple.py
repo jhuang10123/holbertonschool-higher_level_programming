85a77fcd45f8046c6d60b84cf7a2166198645cb6
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-08-07 14:59:23 -0700

    task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+
+
+def add_tuple(tuple_a=(), tuple_b=()):
+
+    sumlist = []
+    a = list(tuple_a)
+    b = list(tuple_b)
+
+    list_a = a.extend([0, 0])
+    list_b = b.extend([0, 0])
+
+    for i in range(2):
+        sumlist.append(a[i] + b[i])
+
+    return tuple(sumlist)
