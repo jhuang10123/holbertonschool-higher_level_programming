822af0ff3ecb91c696d12371849a843058e53a20
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-08-07 15:15:44 -0700

    task 10

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,1 +1,14 @@
 #!/bin/usr/python3
+
+
+def divisible_by_2(my_list=[]):
+
+    new_list = []
+
+    for i in my_list:
+        if i % 2 == 0:
+            new_list += [True]
+        else:
+            new_list += [False]
+
+    return new_list
