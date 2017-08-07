c24a554ec53ab903427d3b8e955e894584ea5bd6
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-08-07 15:44:19 -0700

    list comprehension

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,5 @@
 
 def print_matrix_integer(matrix=[[]]):
 
-    for i in matrix:
-        for j in i:
-            print("{:d}".format(j), end="")
-        print()
+    for row in matrix:
+        print(" ".join('{:d}'.format(element) for element in row))
