eacfc12a0cf6a777e5312f354105e77249f96109
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-08-09 02:31:26 -0700

    structure

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
 
+
+def search_replace(my_list, search, replace):
+    list = []
+
+    list += [replace if my_list[i] == search else my_list[i] for i in my_list]
+    return list
