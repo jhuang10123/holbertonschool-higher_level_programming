1f5cbbeeaac31d80893815b0e465347e966c585e
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-08-09 04:32:02 -0700

    setup

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -7,15 +7,3 @@ def best_score(my_dict):
         return sorted(my_dict.keys())[-1]
 
     return None
-
-
-"""
-    if len(my_dict.keys()) == 0:
-        return None
-
-    return sorted(my_dict.keys())[-1]
-
-    sorted(my_dict)
-
-    return my_dict.keys()[-1]
-"""
--- 0x04-python-more_data_structures/11-mutiply_list_map.py
+++ 0x04-python-more_data_structures/11-mutiply_list_map.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
 
+
+def mutiply_list_map(my_list=[], number=0):
+
+    
