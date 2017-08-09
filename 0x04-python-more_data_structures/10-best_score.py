e08409a6345918ca41d82c7da70183f6b5493f76
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-08-09 04:25:30 -0700

    test

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,2 +1,21 @@
 #!/usr/bin/python3
 
+
+def best_score(my_dict):
+
+    while my_dict:
+        return sorted(my_dict.keys())[-1]
+
+    return None
+
+
+"""
+    if len(my_dict.keys()) == 0:
+        return None
+
+    return sorted(my_dict.keys())[-1]
+
+    sorted(my_dict)
+
+    return my_dict.keys()[-1]
+"""
