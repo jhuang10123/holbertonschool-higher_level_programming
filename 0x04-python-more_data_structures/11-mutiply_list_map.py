e15bff4127ad50f5771849e800426346c13a687c
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-08-11 16:16:30 -0700

    rm copy of list

--- 0x04-python-more_data_structures/11-mutiply_list_map.py
+++ 0x04-python-more_data_structures/11-mutiply_list_map.py
@@ -3,8 +3,4 @@
 
 def mutiply_list_map(my_list=[], number=0):
 
-    result = my_list.copy()
-
-    result = list(map((lambda x: x*number), my_list))
-
-    return result
+    return list(map((lambda x: x*number), my_list))
