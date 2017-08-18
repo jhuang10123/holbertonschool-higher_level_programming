d595c9b5693887469a98949d8a0018a159d84a05
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-08-18 11:50:27 -0700

    task 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        value = a / b
+
+    except:
+        value = None
+
+    finally:
+        print("Inside result:{}".format(value))
