1a0df7d8ab0c5438a8d78623d40ff5048379ad4b
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-08-19 11:41:03 -0700

    fix

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -12,7 +12,7 @@ class Square:
             raise TypeError("size must be an integer")
 
         if type(position) == tuple and len(position) == 2\
-           and type(position[0]) == int and type(position[1] == int)\
+           and type(position[0]) == int and type(position[1]) == int\
            and position[0] >= 0 and position[1] >= 0:
             self.__position = position
 
