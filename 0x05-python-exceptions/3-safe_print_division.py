056385dcd0227c2cc9d033813ddaa998a4f7c47b
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-08-19 10:39:03 -0700

    spacing

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -7,6 +7,6 @@ def safe_print_division(a, b):
         value = None
 
     finally:
-        print("Inside result:{}".format(value))
+        print("Inside result: {}".format(value))
 
     return value
