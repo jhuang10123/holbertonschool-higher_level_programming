77f37d218184def150ad0632e060de79f1507951
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-08-18 23:48:03 -0700

    setup

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+class Square:
+    """ Square class """
+    def __init__(self, size=0):
+        """ size of square(private) """
+        if type(size) == int:
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        """ finds area """
+        return (self.size**2)
