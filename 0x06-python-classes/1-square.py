ae353ef17d738ada1953ead423b37765141d01c9
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-08-18 23:40:29 -0700

    task 2

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 class Square:
     """ Square class """
-    def __init__(self, size):
+    def __init__(self, size=0):
         """ size of square(private) """
         self.__size = size
+
