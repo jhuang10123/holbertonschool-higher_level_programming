acba17b93e20b8a4f975b24c26573f33426c0590
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-08-20 05:48:31 -0700

    tasks 0, 2

--- 0x07-python-test_driven_development/.#0-add_integer.py
+++
@@ -1,1 +0,0 @@
-vagrant@vagrant-ubuntu-trusty-64.4677:1503596113
--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,4 +1,29 @@
 #!/usr/bin/python3
 
 def matrix_divided(matrix, div):
-    
+    """ returns new matrix with each element divided by div """
+
+    new_matrix = []
+    list_len = len(matrix[0])
+
+    if type(div) != int and type(div) != float:
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    for row in matrix:
+        temp = []
+        if len(row) != list_len:
+            raise TypeError("Each row of the matrix must have the same size")
+
+        for i in row:
+            if type(i) == int or type(i) == float:
+                res = i/div
+                i = round(res, 2)
+                temp.append(i)
+            else:
+                raise TypeError("matrix must be a matrix\
+                (list of lists) of integers/floats")
+        new_matrix.append(temp)
+
+    return new_matrix
