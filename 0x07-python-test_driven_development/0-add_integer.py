6f7e57f046af20c1597241e63203739175cf6a9a
Jennifer Huang <133@holbertonschool.com>
2017-08-24 18:11:08 +0000

    setup

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 
 def add_integer(a, b):
-    
+	""" Returns sum of 2 integers """
+	if type(a) != int and type(a) != float:
+		raise TypeError("a must be an integer")
+	if type(b) != int and type(b) != float:
+		raise TypeError("b must be an integer")
+	if (a or b) == float:
+		return int(a) + int(b)
+	else:
+		return a + b
