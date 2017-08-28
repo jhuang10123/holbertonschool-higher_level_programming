651fa5446967c0cf8add351920c4a049e93b2b5d
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-08-28 09:43:12 -0700

    fixed reference

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -4,10 +4,12 @@
 class Rectangle:
     """ rectangle class """
 
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-        number_of_instances = 0
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -58,10 +60,9 @@ class Rectangle:
 
     def __repr__(self):
         """ return a string representation of the rectangle"""
-        number_of_instances += 1
         return("Rectangle({:d}, {:d})".format(self.__width, self.__height))
 
     def __del__(self):
         """del instance of rectangle"""
-        number_of_instances -= 1
         print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
