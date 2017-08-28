efeb151ab2ca668bc8e7de642c829be0337134d6
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-08-28 10:26:42 -0700

    shortened comparison

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -71,11 +71,12 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
+        """ returns rectangle with bigger area """
         if type(rect_1) != Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
         if type(rect_2) != Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1 == rect_2 or rect_1 > rect_2:
-            return rect_1
+        if rect_1.area() >= rect_2.area():
+            return rect_1.area()
         else:
-            return rect_2
+            return rect_2.area()
