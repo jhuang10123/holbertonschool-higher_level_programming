366c119919d353197944a2935150828f5069ed1e
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-08-28 09:11:49 -0700

    added newline

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -50,6 +50,6 @@ class Rectangle:
         if self.__height == 0 or self.__width == 0:
             return ("")
 
-        str = ''.join(('#' * self.__width) * self.__height)
+        str = ''.join(('#' * self.__width + '\n') * self.__height)
 
         return str
