5668ecf364115aaa01e4184cc74586b8c1f61da7
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-08-29 15:37:01 -0700

    error msg edit

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -9,7 +9,7 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-        Rectangle.number_of_instances += 1
+        type(self).number_of_instances += 1
 
     @property
     def width(self):
@@ -65,4 +65,4 @@ class Rectangle:
     def __del__(self):
         """del instance of rectangle"""
         print("Bye rectangle...")
-        Rectangle.number_of_instances -= 1
+        type(self).number_of_instances -= 1
