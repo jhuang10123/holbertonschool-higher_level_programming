5cdc483b3ce61b042464c5843d0c64598ec200d3
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-08-29 17:26:01 -0700

    Revert "  rm comments"
    This reverts commit af83fe1eb6c7f79d0474e6316d9582f532bfe364.

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -10,3 +10,17 @@ def print_square(size):
         for j in range(0, size):
             print("#".format(), end="")
         print()
+
+
+#!/usr/bin/python3
+
+
+# def print_square(size):
+#     if type(size) == float and size < 0:
+#         raise TypeError("size must be an integer")
+#     if not isinstance(size, int):
+#         raise TypeError("size must be an integer")
+#     if size < 0:
+#         raise ValueError("size must be >= 0")
+#     for i in range(size):
+#         print ('#' * size)
