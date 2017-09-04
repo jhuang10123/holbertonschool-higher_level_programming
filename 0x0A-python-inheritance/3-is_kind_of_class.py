c7ae77e146e4be1178f8fa5751d5182ca4aa9598
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-09-04 02:59:12 -0700

    edited function name

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def is_same_class(obj, a_class):
+def is_kind_of_class(obj, a_class):
     """ returns whether object is exactly an instance of the specified class """
     return isinstance(obj, a_class)
