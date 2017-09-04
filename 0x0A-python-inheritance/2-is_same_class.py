1a618483d45705d6e147b04db0080979ef699997
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-09-04 02:56:40 -0700

    commented out isinstance

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,1 +1,5 @@
 #!/usr/bin/python3
+def is_same_class(obj, a_class):
+    """ returns whether object is exactly an instance of the specified class """
+    return type(obj) is a_class
+#    return isinstance(obj, a_class)
