6e3096628a2ee50a741341ef6882be957e8950a3
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-09-04 02:57:58 -0700

    pep8

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+
+
 """class MyList that inherits from list"""
 class MyList(list):
     """ prints sorted list """
--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def is_same_class(obj, a_class):
-    """ returns whether object is exactly an instance of the specified class """
+    """ returns whether object is exactly an instance of specified class """
     return type(obj) is a_class
-#    return isinstance(obj, a_class)
