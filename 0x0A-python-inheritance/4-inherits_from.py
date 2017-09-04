1325b4345ccd8823f6d4eb8e3a676135d980f8e7
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-09-04 03:07:12 -0700

    task4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,1 +1,4 @@
 #!/usr/bin/python3
+def inherits_from(obj, a_class):
+    """ returns whether object is instance inherited from specified class """
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
