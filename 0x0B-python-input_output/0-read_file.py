ce8c0510da5ffba3e6d64d40c055175927a9f65f
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-09-05 16:38:57 -0700

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    with open(filename, 'r', encoging="UTF8") as file:
+    with open(filename, 'r', encoding="UTF8") as file:
         rd_file = file.read()
         for line in rd_file:
-            print(line)
+            print(line, end="")
