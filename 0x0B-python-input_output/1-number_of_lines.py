58659b5789c14f3723be1ce73ad61b3529f495cd
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-09-05 16:52:18 -0700

    task 1

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def number_of_lines(filename=""):
+    with open(filename, 'r', encoding="UTF8") as file:
+        count = 0
+        for line in file:
+            count+=1
+    return count
