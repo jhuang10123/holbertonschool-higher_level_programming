417ad53bf0c8f51b9c1c32fc95fae791c2367c8b
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-09-05 19:11:28 -0700

    task 9 structure

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -3,5 +3,6 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
+    """writes an Object to a text file, using a JSON representation"""
     with open(filename, 'w', encoding="UTF8") as file:
         json.dump(my_obj, file)
--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+import sys
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+alist = []
+if len(sys.argv) > 1:
+    alist = [item for item in sys.argv]
+new_list = load_from_json_file(alist)
+save_to_json_file(new_list, filename)
