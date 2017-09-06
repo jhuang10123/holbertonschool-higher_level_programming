5f7608cd368e5097a8630d50cb68b256977f7688
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-09-05 17:51:29 -0700

    task 4

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-
-with open(filename, 'r', encoding="UTF8") as file:
+def append_write(filename="", text=""):
+    with open(filename, 'a', encoding="UTF8") as file:
+        return file.write("{}".format(text))
