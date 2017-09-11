1bb4097836492c107a003bfc1c1bb8e4f986d83a
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-09-11 00:16:33 -0700

    added error handling

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -18,6 +18,10 @@ class Square(Rectangle):
 
     @size.setter
     def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value <= 0:
+            raise ValueError("size must be > 0")
         self.width = value
         self.height = value
 
