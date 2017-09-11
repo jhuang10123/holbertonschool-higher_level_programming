cdf431d7cbf9ce6db5b37f5fc81eba439d31dfd1
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-09-11 00:46:54 -0700

    dict to json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+import json
+
+
 class Base:
     __nb_objects = 0
     def __init__(self, id=None):
@@ -7,3 +10,8 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        if list_dictionaries == None:
+            return "[]"
+        return json.dumps(list_dictionaries)
