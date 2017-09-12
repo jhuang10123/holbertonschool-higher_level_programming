2e0c3b8a9f08d2655b187fbb62b0e260c80316c2
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-09-12 14:06:16 -0700

    fixed comma

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -76,5 +76,5 @@ class Base:
 
 # update each element, add to list
         for elem in file:
-            new_list.append(cls, create(**elem))
+            new_list.append(cls.create(**elem))
         return new_list
