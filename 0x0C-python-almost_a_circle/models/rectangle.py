d0cdead287ba321d0e3ac0dddd66605ed939d7b4
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-09-11 22:31:35 -0700

    indentation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -82,8 +82,8 @@ class Rectangle(Base):
 
     def __str__(self):
         """ updating __str__ """
-        return ("[Rectangle] {} {}/{} - {}/{}".format(self.id, self.__x,
-                                                      self.__y, self.__width, self.__height))
+        return ("[Rectangle] {} {}/{} - {}/{}".format(self.id, self.__x, \
+                self.__y, self.__width, self.__height))
 
 """
     def update(self, *args, **kwargs):
