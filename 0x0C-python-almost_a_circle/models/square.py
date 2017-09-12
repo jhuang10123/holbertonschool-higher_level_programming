edc014cc7c54feb2110d14337f067de929e32dd0
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-09-11 22:57:30 -0700

    doc

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -18,10 +18,12 @@ class Square(Rectangle):
 
     @property
     def size(self):
+        """ getter for size"""
         return self.width
 
     @size.setter
     def size(self, value):
+        """ setter for size """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value <= 0:
