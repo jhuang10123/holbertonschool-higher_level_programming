e6ab9179d0d845c37d1def24fc59c768e0082aa8
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-10-06 15:30:25 -0700

    5 edit

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,5 +1,4 @@
 -- prints the full description of the table first_table
 -- from the database hbtn_0c_0 in your MySQL server.
 -- database name will be passed as an argument of the mysql command
-SELECT * FROM first_table
-SHOW CREATE TABLE
+SHOW CREATE TABLE first_table
