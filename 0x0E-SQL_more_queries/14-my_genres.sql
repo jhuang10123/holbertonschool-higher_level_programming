ef6712dcdef37db2582eceda62c9a9d082062f17
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-10-07 07:47:40 -0700

    edit

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,1 +1,5 @@
 -- uses the hbtn_0d_tvshows database to lists all genres of the show Dexter.
+SELECT tv_genres.name
+FROM tv_genres
+JOIN tv_shows WHERE tv_shows.title="DEXTER"
+ORDER BY tv_genres.name ASC;
