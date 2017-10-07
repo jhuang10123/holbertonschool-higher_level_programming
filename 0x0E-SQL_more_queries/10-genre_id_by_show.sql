ef04ed00b49b2ba9a13986cd6420aea055798f1a
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-10-07 07:15:46 -0700

    test#1983517137

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,6 @@
 --  lists all shows contained in hbtn_0d_tvshows with at least one genre linked.
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows_genres
-JOIN tv_shows
+FROM tv_shows
+JOIN tv_show_genres
 ON tv_show_genres.genre_id=tv_shows.id
 ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
