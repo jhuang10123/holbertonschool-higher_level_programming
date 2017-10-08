93d0a7b9e4fa12dacb7b3fd29368d6c02618f59e
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-10-07 18:21:57 -0700

    up to 16

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,2 +1,6 @@
 --  lists all genres from hbtn_0d_tvshows
 -- and displays the number of shows linked to each.
+SELECT `tv_genres.name`, COUNT(*) as `number of shows`
+FROM 
+WHERE `tv_genres.show_id` NOT NULL;
+ORDER BY tv_genres.name DESC, number of shows DESC;
