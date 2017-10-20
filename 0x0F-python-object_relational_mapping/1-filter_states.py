59300287b42a510da1e0802c0b65c9c9eeedf59d
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-10-20 12:37:53 -0700

    0

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+"""
+lists all states with a name starting with N (upper N) from database
+"""
+import MySQLdb
+
---
+++ 0x0F-python-object_relational_mapping/test.py
@@ -0,0 +1,7 @@
+#!/bin/bash/python3
+
+"""
+1 - lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
+"""
+
+SELECT 'name' FROM 'states'
