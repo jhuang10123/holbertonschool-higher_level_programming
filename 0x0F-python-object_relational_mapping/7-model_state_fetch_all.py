08c344982cfcf4776680416bfb199264c0fd4388
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-10-20 16:47:39 -0700

    task 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""
+lists all State objects from the database hbtn_0e_6_usa
+"""
+from model_state import State, Base
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(argv[1], argv[2], argv[3]))
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+for state in session.query(State).order_by(State.id).all():
+    print("{}: {}".format(state.id, state.name))
+session.close()
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """
+class definition of a State and an instance Base = declarative_base()
 """
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, Numeric, String
 
 Base = declarative_base()
 
+
 class State(Base):
+    """ class inherited from Base """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
