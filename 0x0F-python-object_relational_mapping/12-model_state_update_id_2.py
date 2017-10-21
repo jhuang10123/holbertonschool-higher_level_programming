a3303809b4a2f65f88b397336e9550dfd85869a4
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-10-20 18:44:10 -0700

    task 12

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""
+changes the name of a State object from the database hbtn_0e_6_usa
+"""
+from model_state import State, Base
+from sys import argv
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    ce = ('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(argv[1],
+                                                           argv[2],
+                                                           argv[3]))
+    engine = create_engine(ce)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    update = session.query(State).filter_by(id=2).first()
+    update.name = "New Mexico"
+    session.commit()
+    session.close()
