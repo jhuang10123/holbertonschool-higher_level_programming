c7814881cfd68cecd953b376bab90b6dc15a33e5
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-11-22 00:15:47 +0000

    rm GET

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # sends a GET request to URL and displays the body of the response
 # header variable X-HolbertonSchool-User-Id=98
-curl "$1" -X GET -sH "X-HolbertonSchool-User-Id: 98" 
+curl "$1" -sH "X-HolbertonSchool-User-Id: 98" 
