28aa0808cf1495ad87f45d04899b42b88967c141
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-11-22 08:33:53 +0000

    correct 102

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
-!#/bin/bash
-# that causes the server to respond with a message containing "You got me!"
-curl  0.0.0.0:5000/catch_me 
+#!/bin/bash
+# that causes the server to respond with a message
+curl  -sLH "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me -X PUT -d "user_id=98"
