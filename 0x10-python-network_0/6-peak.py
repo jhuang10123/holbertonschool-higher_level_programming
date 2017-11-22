653cf712daed58b2b1fa762b9e581e2f9bb661be
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-11-22 05:57:25 +0000

    test

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,6 +3,8 @@
 
 
 def find_peak(list_of_integers):
+    if list_of_integers is None:
+        return None
     for idx, val in enumerate(list_of_integers):
         if idx != 0 and idx != len(list_of_integers) - 1:
             if list_of_integers[idx - 1] < val > list_of_integers[idx + 1]:
