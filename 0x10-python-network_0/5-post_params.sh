6f1d899a9556a9610bfad46b423e8f6fa402f185
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-11-22 00:21:15 +0000

    rm comments, pep8

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # sends a POST request to URL and displays the body of the response
-# email = hr@holbertonschool.com
-# subject = "I will always be here for PLD"
 curl "$1" -sd email=hr@holbertonschool.com -d subject="I will always be here for PLD"
--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ finds a peak in a list of unsorted integers """
+
+
 def find_peak(list_of_integers):
     for idx, val in enumerate(list_of_integers):
         if list_of_integers[idx + 1] < val and idx != 0:
