5f3bc8edb4bd294c029b1370fd3bb141daa87105
Jennifer Huang <jhuang1012bn@yahoo.com>
2017-11-27 20:00:05 +0000

    task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+
+"""
+import urllib.request
+import urllib.parse
+
+if __name__ == "__main__":
+
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        dc = html.decode('utf-8')
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(dc))
