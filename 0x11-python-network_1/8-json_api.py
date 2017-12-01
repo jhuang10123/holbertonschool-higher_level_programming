b31d82e675db4862c2bf43f6be79f21dfc688613
jhuang10123 <133@holbertonschool.com>
2017-12-01 21:16:54 +0000

    task 10

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,17 +1,34 @@
 #!/usr/bin/python3
 """
-takes in a letter and sends a POST request to
+takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the
+letter as a parameter
 """
 import requests
 import sys
 
 
 if __name__ == "__main__":
-    url = "http://0.0.0.0:5000/"
-    if sys.argv[1] is None:
-        q = ""
-    else:
-        q = sys.argv[1]
-    param = {'search_user': q}
-    req = requests.post(url, params=param)
-    print (req)
+    url = "http://0.0.0.0:5000/search_user"
+    q=""
+    if len(sys.argv) > 1:
+        print("pass argv")
+        q=sys.argv[1]
+
+    data = {'q':q}
+
+    req = requests.post(url, params=data)
+
+    try:
+        req_jsn = req.json()
+        if len(req_jsn) == 0:
+            print("No result")
+        # if req_json == {} or 'name' not in req_json:
+        #     print("No result")
+        # else:
+        #     print("[{}] {}".format(req_jsn['id'],req_jsn['name']))
+    except:
+            print("Not a valid JSON")
+
+
+
