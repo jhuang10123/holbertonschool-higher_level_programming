0aba29ff6aec611c7041f3d904a67033dbc7d3f7
jhuang10123 <133@holbertonschool.com>
2018-01-03 22:10:58 +0000

    task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// prints a square"
+let sizeOfSqr = parseInt(process.argv[2]);
+if (!sizeOfSqr) {
+  console.log('Missing size');
+}
+for (let i = 0; i < sizeOfSqr; i++) {
+  console.log('x'.repeat(sizeOfSqr));
+}
