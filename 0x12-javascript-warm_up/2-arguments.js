40948e9c2b0b723b8596720753f14e55b73a12bc
jhuang10123 <133@holbertonschool.com>
2018-01-03 21:15:16 +0000

    task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,3 @@
+#!/usr/bin/node
+// prints a message depending of the number of arguments passed
+if (!process.argv[2]) { console.log('No argument'); } else if (process.argv[3]) { console.log('Arguments found'); } else { console.log('Argument found'); }
