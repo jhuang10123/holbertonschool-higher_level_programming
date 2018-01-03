3e39207e64f9e87dbe657323bfd730379cdafd4c
jhuang10123 <133@holbertonschool.com>
2018-01-03 22:39:53 +0000

    task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// prints factorial of a given number
+let n = parseInt(process.argv[2])
+function factorial(n){
+  if (!n || n == 1){
+    return (1);}
+return (n * factorial(n-1))
+}
+
+console.log(factorial(n))
