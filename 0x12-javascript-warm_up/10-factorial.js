1fc3611560f478932c5af8a66e1f7d8920a4dc9f
jhuang10123 <133@holbertonschool.com>
2018-01-04 17:01:55 +0000

    semistandard

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,7 +2,7 @@
 // prints factorial of a given number
 let n = parseInt(process.argv[2]);
 function factorial (n) {
-  if (!n || n == 1) {
+  if (!n || n === 1) {
     return (1);
   }
   return (n * factorial(n - 1));
