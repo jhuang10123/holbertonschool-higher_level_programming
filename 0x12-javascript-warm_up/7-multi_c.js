8a92e6269c37f26ce5169898d18de992f9fdef14
jhuang10123 <133@holbertonschool.com>
2018-01-04 01:00:06 +0000

    fixing for loop

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,6 +4,6 @@ let numOfTimes = parseInt(process.argv[2]);
 if (!numOfTimes) {
   console.log('Missing number of occurrences');
 }
-for (let i = 0; i <= numOfTimes; i++) {
+for (let i = 0; i < numOfTimes; i++) {
   console.log('C is fun');
 }
