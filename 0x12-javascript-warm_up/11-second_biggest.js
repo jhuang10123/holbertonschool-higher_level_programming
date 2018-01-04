c24e27821a28239d4e96f39f246b3b4b27508c25
jhuang10123 <133@holbertonschool.com>
2018-01-04 00:58:32 +0000

    adding tasks 4, 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// searches the second biggest integer in the list of arguments
+if (!process.argv[2] || !process.argv[3]) { console.log(0); } else {
+// store args in a list
+  let list = [];
+  for (let i = 0; process.argv[i]; i++) {
+    list.push(process.argv[i]);
+  }
+  // sort list from least to greatest
+  list.sort();
+  // find 2nd to last element in list
+  idx = list.length - 2;
+  console.log(list[idx]);
+}
---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+// Prints two arguments passed to it
+// in the following format: “ is ”prints first argument passed
+console.log(process.argv[2] + ' is ' + process.argv[3]);
