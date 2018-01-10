18a6876d4daf65364f84152a56433fe82dc27c2f
= <=>
2018-01-10 23:28:18 +0000

    task 5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,5 @@
 #!/usr/bin/node
+//ets the contents of a webpage and stores it in a file
+const request = require('request');
+const fs = require('fs');
+request(process.argv[2], 'utf8').pipe(fs.createWriteStream(process.argv[3]));
