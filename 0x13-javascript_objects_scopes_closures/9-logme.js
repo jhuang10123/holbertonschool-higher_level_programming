a103784b11e3bd7ed6d1dc66efb347c68f8c6ef9
= <=>
2018-01-10 04:32:49 +0000

    test

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 let count = 0;
 exports.logMe = function (item) {
-   let fcn = function () {
+//   let fcn = function () {
     console.log(count + ':' + item);
     count++;
-  };
- return fcn();
+//  };
+// return fcn();
 };
