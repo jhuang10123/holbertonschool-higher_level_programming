9e21e9a766161ab244b528cbb17e6a7b323f2751
= <=>
2018-01-10 04:03:15 +0000

    edit class name

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,32 +1,7 @@
 #!/usr/bin/node
-// class Rectangle that defines a rectangle
-// module.exports = class Rectangle {
-// class Rectangle {
-//   constructor (w, h) {
-//     if (h > 0 && w > 0) {
-//       this.height = h;
-//       this.width = w;
-//     }
-//   }
-//   print () {
-//     for (let i = 0; i < this.height; i++) {
-//       console.log('X'.repeat(this.width));
-//     }
-//   }
-//   rotate () {
-//     let temp = this.width;
-//     this.width = this.height;
-//     this.height = temp;
-//   }
-//   double () {
-//     this.width *= 2;
-//     this.height *= 2;
-//   }
-// }
-
 const Rectangle = require('./4-rectangle');
 
-module.exports = class square extends Rectangle {
+module.exports = class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
