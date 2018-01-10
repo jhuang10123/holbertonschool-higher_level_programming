b713cedb3cf39d65093efb53b6d19f4642ac2836
= <=>
2018-01-10 21:32:30 +0000

    semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
 // reads and prints the content of a file
 
-//syntax for including the File System module in appplication
+// syntax for including the File System module in appplication
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf8', function(err, data) {
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
   if (err) {
     console.log(err);
-  }
-  else {
+  } else {
     console.log(data);
   }
 });
--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-//display the status code of a GET request
+// display the status code of a GET request
 const request = require('request');
-const url = process.argv[2]
+const url = process.argv[2];
 request
   .get(url)
-  .on('response', function(response) {
-    console.log("code: " , response.statusCode)
-});
+  .on('response', function (response) {
+    console.log('code: ', response.statusCode);
+  });
