c88bb9c19e54951bb17481a30b51d9b84204fc8f
= <=>
2018-01-10 04:03:29 +0000

    rm commented out code

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,35 +1,4 @@
 #!/usr/bin/node
-// class Rectangle that defines a rectangle
-// module.exports = class Rectangle {
-// class Rectangle {
-//   constructor (w, h) {
-//     if (h > 0 && w > 0) {
-//       this.height = h;
-//       this.width = w;
-//     }
-//   }
-//   print () {
-//     for (let i = 0; i < this.height; i++) {
-//       console.log('X'.repeat(this.width));
-//     }
-//   }
-//   rotate () {
-//     let temp = this.width;
-//     this.width = this.height;
-//     this.height = temp;
-//   }
-//   double () {
-//     this.width *= 2;
-//     this.height *= 2;
-//   }
-// };
-
-// class square1 extends Rectangle {
-//   constructor(size) {
-//     super(size, size);
-//   }
-// };
-
 const Square1 = require('./5-square');
 module.exports = class Square extends Square1 {
   charPrint (c) {
