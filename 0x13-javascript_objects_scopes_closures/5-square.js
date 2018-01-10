81cf94d7a296071640db84172da12ced8bac1ea5
= <=>
2018-01-10 00:59:09 +0000

    task 5

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+// class Rectangle that defines a rectangle
+//module.exports = class Rectangle {
+class Rectangle {
+  constructor (w, h) {
+    if (h > 0 && w > 0) {
+      this.height = h;
+      this.width = w;
+    }
+  }
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+  rotate () {
+    let temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
+
+module.exports = class square extends Rectangle {
+  constructor(size) {
+    super(size, size);
+  }
+}
+
