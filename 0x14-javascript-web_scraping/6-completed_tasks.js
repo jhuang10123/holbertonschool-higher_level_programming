5d5cec08601187b75066668d442a65a2c6340021
= <=>
2018-01-11 00:05:42 +0000

    rm url

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,28 +1,25 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://jsonplaceholder.typicode.com/todos'
 request
-  .get(url, function(err, response, body) {
+  .get(process.argv[2], function (err, response, body) {
     if (err) {
       console.log(err);
-    }
-    else {
+    } else {
       let newDict = {};
       // get list of all data
-      allTodos = JSON.parse(body);
-      
-      for (item in allTodos) {
-	let each = allTodos[item];
-	//check whether user id already in newDict
-	if (each.completed === true) {
-	  if (each.userId in newDict) {
-	    newDict[each.userId]++;
-	  }
-	  else {
-	    newDict[each.userId] = 1;
-	  }
-	}
+      const allTodos = JSON.parse(body);
+
+      for (let item in allTodos) {
+        let each = allTodos[item];
+        // check whether user id already in newDict
+        if (each.completed === true) {
+          if (each.userId in newDict) {
+            newDict[each.userId]++;
+          } else {
+            newDict[each.userId] = 1;
+          }
+        }
       }
       console.log(newDict);
-
-    }});
+    }
+  });
