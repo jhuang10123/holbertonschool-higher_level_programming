5035771ec7c70b2fa3d7e9bd2901b070705cc96e
= <=>
2018-01-11 00:07:59 +0000

    rm url and extra space

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 // display the status code of a GET request
 const request = require('request');
-const url = process.argv[2];
 request
-  .get(url)
+  .get(process.argv[2])
   .on('response', function (response) {
-    console.log('code: ', response.statusCode);
+    console.log('code:', response.statusCode);
   });
