c3008e69082aa949634686ce9dfa1ccf54e12af7
jhuang10123 <133@holbertonschool.com>
2018-01-15 07:31:08 +0000

    rm unnecessary code

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,20 +1,6 @@
-$(function()
-{
-    $.getJSON('https://swapi.co/api/films/?format=json', function(data){
-      let result = data.results
-      for (let i in result) {
-	$('UL#list_movies').append('<li>' + result[i].title + '</li>');
-	};
-
-
-      // let result = data.results
-      // alert(result);
-
-      // let items = [];
-      // $.each(data, function(key, val) {
-      // 	items.push('<li>' + val + '</li>');
-      // 	});
-      // alert(items);
-
-  });
+$.getJSON('https://swapi.co/api/films/?format=json', function(data){
+  let result = data.results
+  for (let i in result) {
+    $('UL#list_movies').append('<li>' + result[i].title + '</li>');
+  };
 });
